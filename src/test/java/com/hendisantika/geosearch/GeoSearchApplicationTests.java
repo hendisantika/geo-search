5d429c06b944d7d49e72b3879c852a694893579c
Hendi Santika <hendisantika@gmail.com>
2017-12-20 07:29:01 +0700

    #1 - Initial ptoject

---
+++ src/test/java/com/hendisantika/geosearch/GeoSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.geosearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
