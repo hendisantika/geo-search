f1224e6ab91edaf4170a1e8bf624a46c1c325afd
Hendi Santika <hendisantika@gmail.com>
2017-12-20 07:40:34 +0700

    #5 - Modify main App

--- src/main/java/com/hendisantika/geosearch/GeoSearchApplication.java
+++ src/main/java/com/hendisantika/geosearch/GeoSearchApplication.java
@@ -1,12 +1,73 @@
 package com.hendisantika.geosearch;
 
+import com.hendisantika.geosearch.domain.Pub;
+import com.hendisantika.geosearch.repository.PubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Point;
 
 @SpringBootApplication
-public class GeoSearchApplication {
+@ComponentScan
+@EnableAutoConfiguration
+public class GeoSearchApplication implements CommandLineRunner {
+
+    @Autowired
+    private PubRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoSearchApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) {
+
+        repository.deleteAll();
+
+        repository.save(new Pub("George Canning", 3, 51.4678685, -0.0860632));
+        repository.save(new Pub("The Cherry Tree", -1, 51.461512, -0.078988));
+        repository.save(new Pub("The Fox on the Hill", 3, 51.4651705, -0.0895804));
+        repository.save(new Pub("The Flying Pig", 5, 51.461744, -0.070394));
+        repository.save(new Pub("The East Dulwich Tavern", 4, 51.460463, -0.07513));
+
+        System.out.println("Pubs found with findAll():");
+        System.out.println("-------------------------------");
+        for (Pub pub : repository.findAll()) {
+            System.out.println(pub);
+        }
+        System.out.println();
+
+        System.out.println("Pubs found with findByRatingGreaterThan(3):");
+        System.out.println("-------------------------------");
+        for (Pub pub : repository.findByRatingGreaterThan(3)) {
+            System.out.println(pub);
+        }
+        System.out.println();
+
+        System.out.println("Pubs found with findByRatingLessThan(0):");
+        System.out.println("-------------------------------");
+        for (Pub pub : repository.findByRatingLessThan(0)) {
+            System.out.println(pub);
+        }
+        System.out.println();
+
+        System.out.println("Pub found with findByFirstName('The Flying Pig'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByName("The Flying Pig"));
+
+        System.out.println("Pubs found within 1K of '51.4634836,-0.0841914':");
+        System.out.println("--------------------------------");
+        for (GeoResult<Pub> pub : repository.findByLocationNear(new Point(51.4634836, -0.0841914), new Distance(1, Metrics.KILOMETERS))) {
+            System.out.println(pub.getContent());
+        }
+        System.out.println();
+
+    }
+
 }
